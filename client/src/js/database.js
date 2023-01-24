62115ebdd860c33f75ec697587e9d15e2490d80f
moony <moonyxiong@gmail.com>
2023-01-23 18:12:18 -0600

    edited comments

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,7 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+
 export const putDb = async (content) => {
   console.log('PUT to the database');
   const jateDb = await openDB('jate', 1);
@@ -24,7 +24,7 @@ export const putDb = async (content) => {
 };
 
 
-// TODO: Add logic for a method that gets all the content from the database
+
 export const getDb = async () => {
   console.log('GET from the database');
   const jateDb = await openDB('jate', 1);
@@ -35,7 +35,7 @@ export const getDb = async () => {
   result
     ? console.log('🚀 - data retrieved from the database', result.value)
     : console.log('🚀 - data not found in the database');
-  // Check if a variable is defined and if it is, return it. See MDN Docs on Optional Chaining (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining)
+  
   return result?.value;
 };
 
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -17,17 +17,17 @@ butInstall.addEventListener('click', async () => {
    return;
   }
 
-  // Show prompt
+ 
   promptEvent.prompt();
   
-  // Reset the deferred prompt variable, it can only be used once.
+  
   window.deferredPrompt = null;
   
   butInstall.classList.toggle('hidden', true);
 });
 
 window.addEventListener('appinstalled', (event) => {
-  // Clear prompt
+ 
   window.deferredPrompt = null;
 });
 
